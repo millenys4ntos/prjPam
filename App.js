38bf11241c8bb87c8ca7714a7b223944baa327b4
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-02-18 18:35:53 -0300

    inicio do projeto

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.Texto}>Olá mundo!</Text>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -13,8 +13,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FDDDDE',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Texto: {
+    color: '#000'
+  }
 });
