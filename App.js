9620d592e35f8f8e9cfaa87ba11928344c288caf
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-04 17:37:32 -0300

    atualizando o js

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.Texto}>Olá mundo!</Text>
+      <Text style={styles.texto}>Olá mundo!</Text>
       <StatusBar style="auto"/>
+      <View style={styles.bloco}>
+      <Text style={styles.textoB}>Valor 1</Text>
+        <TextInput 
+          style={styles.input}
+          keyboardType= "numeric"
+        />
+      </View>
+      <View style={styles.bloco}>
+      <Text style={styles.textoB}>Valor 2</Text>
+        <TextInput 
+          style={styles.input}
+          keyboardType= "numeric"
+        />
+      </View>
+      <View style={styles.bloco}>
+        <TouchableOpacity style={styles.botao}>
+          <Text style={styles.textoBotao}>Somar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -17,7 +35,42 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  Texto: {
-    color: '#000'
+  texto: {
+    color: '#000',
+    fontSize:30,
+  },
+  textoB: {
+    fontSize: 20,
+    marginTop: '10px',
+    display: 'flex', 
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input:{
+    bordercolor: '#000',
+    borderWidth: 2,
+    fontSize: 30,
+    width: '80%',
+  
+  },
+  bloco: {
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 30,
+    display: 'flex', 
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  botao: {
+    backgroundColor: '#000',
+    width: '80%',
+    textAlign: 'center',
+    marginTop: '12px'
+  },
+  textoBotao: {
+    color: '#fff',
+    fontSize:30,
+    textAlign: 'center',
+    alignItems: 'center'
   }
 });
